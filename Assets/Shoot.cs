073001bd37fd3d40070f05924591f0607b20eb0e
ReturnHao <798273600@qq.com>
2019-02-22 11:47:10 +0900

    Juvenile Project

---
+++ Assets/Shoot.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    CameraSpacePoint QuaternionToEuler(Vector4 q) // Z-Y-X Euler angles
+    {
+        CameraSpacePoint euler = { 0 };
+        const double Epsilon = 0.0009765625f;
+        const double Threshold = 0.5f - Epsilon;
+
+        double TEST = q.w * q.y - q.x * q.z;
+
+        if (TEST < -Threshold || TEST > Threshold) // 奇异姿态,俯仰角为±90°
+        {
+            int sign = Sign(TEST);
+
+            euler.Z = -2 * sign * (double)atan2(q.x, q.w); // yaw
+
+            euler.Y = sign * (PI / 2.0); // pitch
+
+            euler.X = 0; // roll
+
+        }
+        else
+        {
+            euler.X = atan2(2 * (q.y * q.z + q.w * q.x), q.w * q.w - q.x * q.x - q.y * q.y + q.z * q.z);
+            euler.Y = asin(-2 * (q.x * q.z - q.w * q.y));
+            euler.Z = atan2(2 * (q.x * q.y + q.w * q.z), q.w * q.w + q.x * q.x - q.y * q.y - q.z * q.z);
+        }
+
+        return euler;
+    }
+    public GameObject bullet;
+    public float speed = 5f;
+
+    private int ballIndex = 0;
+    private List<GameObject> objBall;
+
+    private List<Joycon> joycons;
+
+    // Values made available via Unity
+    public float[] stick;
+    public Vector3 gyro, lastgyro;
+    public Vector3 accel;
+    public int jc_ind = 0;
+    public Quaternion orientation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objBall = new List<GameObject>();
+
+        gyro = new Vector3(0, 0, 0);
+        accel = new Vector3(0, 0, 0);
+        // get the public Joycon array attached to the JoyconManager in scene
+        joycons = JoyconManager.Instance.j;
+        if (joycons.Count < jc_ind + 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // make sure the Joycon only gets checked if attached
+        if (joycons.Count > 0)
+        {
+            Joycon j = joycons[jc_ind];
+            // GetButtonDown checks if a button has been pressed (not held)
+            if (j.GetButtonDown(Joycon.Button.HOME))
+            {
+                ballIndex++;
+                GameObject ball = Instantiate(bullet, transform.position, transform.rotation);
+                ball.name = "Ball" + ballIndex;
+                objBall.Add(ball);
+
+                Rigidbody rig = ball.GetComponent<Rigidbody>();
+
+                rig.velocity = transform.forward * speed;
+                if (ballIndex > 5)
+                {
+                    Destroy(objBall[0]);
+                    objBall.RemoveAt(0);
+                }
+            }
+            if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+            {
+                Debug.Log("Shoulder button 2 pressed");
+                // GetStick returns a 2-element vector with x/y joystick components
+                Debug.Log(string.Format("Stick x: {0:N} Stick y: {1:N}", j.GetStick()[0], j.GetStick()[1]));
+
+                // Joycon has no magnetometer, so it cannot accurately determine its yaw value. Joycon.Recenter allows the user to reset the yaw value.
+                j.Recenter();
+            }
+            // GetButtonDown checks if a button has been released
+            if (j.GetButtonUp(Joycon.Button.SHOULDER_2))
+            {
+                Debug.Log("Shoulder button 2 released");
+            }
+            // GetButtonDown checks if a button is currently down (pressed or held)
+            if (j.GetButton(Joycon.Button.SHOULDER_2))
+            {
+                Debug.Log("Shoulder button 2 held");
+            }
+
+            if (j.GetButtonDown(Joycon.Button.DPAD_DOWN))
+            {
+                Debug.Log("Rumble");
+
+                // Rumble for 200 milliseconds, with low frequency rumble at 160 Hz and high frequency rumble at 320 Hz. For more information check:
+                // https://github.com/dekuNukem/Nintendo_Switch_Reverse_Engineering/blob/master/rumble_data_table.md
+
+                j.SetRumble(160, 320, 0.6f, 200);
+
+                // The last argument (time) in SetRumble is optional. Call it with three arguments to turn it on without telling it when to turn off.
+                // (Useful for dynamically changing rumble values.)
+                // Then call SetRumble(0,0,0) when you want to turn it off.
+            }
+
+            stick = j.GetStick();
+
+            // Gyro values: x, y, z axis values (in radians per second)
+            lastgyro = gyro;
+            gyro = j.GetGyro();
+
+            //Debug.Log(gyro.x);
+
+            if ((gyro.x >= 0.5) && (lastgyro.x <= -0.5))
+            {
+                Debug.Log("Fire");
+                j.SetRumble(160, 320, 0.6f, 200);
+                ballIndex++;
+                GameObject ball = Instantiate(bullet, transform.position, transform.rotation);
+                ball.name = "Ball" + ballIndex;
+                objBall.Add(ball);
+
+                Rigidbody rig = ball.GetComponent<Rigidbody>();
+                rig.velocity = transform.forward * speed;
+
+                if (ballIndex > 5)
+                {
+                    Destroy(objBall[0]);
+                    objBall.RemoveAt(0);
+                }
+            }
+
+            // Accel values:  x, y, z axis values (in Gs)
+            accel = j.GetAccel();
+
+            orientation = j.GetVector();
+            if (j.GetButton(Joycon.Button.DPAD_UP))
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+            }
+            else
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            }
+            gameObject.transform.rotation = orientation;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Magic_Reality_Warriors
 RM2C Lab Juvenile Project: MIX REALITY GAME
+
+## Change Log
+
+### Fri Feb 22 2019 on Return@MacOS
+
+    - Project Uploaded.
